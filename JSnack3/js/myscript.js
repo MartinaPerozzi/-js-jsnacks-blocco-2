990cf91452c5be37e7add51b7ec4aa6823766650
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-27 16:33:27 +0100

    JSnack3 - html + css + js -structure

---
+++ JSnack3/js/myscript.js
@@ -0,0 +1,9 @@
+// Creo un array di numeri
+const numbersArr = [10, 15, 35, 23, 89, 45, 21, 74, 85];
+
+// Seleziono solo i numeri dispari
+
+const odds = numbersArr.filter((num) => num % 2 === 1);
+console.log(odds);
+
+// 
