ee66a05de58cf5ccf8dcfe57e5e37a85fa2195f8
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-27 15:30:58 +0100

    JSnack2 - html + css + js - in progress

---
+++ JSnack2/js/myscript.js
@@ -0,0 +1,35 @@
+// Prendo i miei elementi
+const textRed = document.querySelector("#red-color");
+console.log(textRed);
+const textGreen = document.querySelector("#green-color");
+// Button
+const buttonProceed = document.querySelector(".btn-number");
+
+// Creo un'array di numeri
+const numberArray = [10, 30, 28, 45, 89, 21, 35, 88, 12, 3];
+const boxElements = document.querySelector(".box-elements");
+boxElements.innerHTML += "10, 30, 28, 45, 89, 21, 35, 88, 12, 3";
+
+
+
+buttonProceed.addEventListener(
+    "click",
+    function () {
+
+        for (let i = 0; i < numberArray.length; i++) {
+            const currentNumber = numberArray[i];
+
+
+            if (currentNumber % 2 == 0) {
+
+                textRed.innerHTML += currentNumber;
+                console.log(textRed);
+
+            } else if (currentNumber % 2 != 0) {
+                textGreen.innerHTML += currentNumber;
+
+            }
+        }
+    }
+)
+
